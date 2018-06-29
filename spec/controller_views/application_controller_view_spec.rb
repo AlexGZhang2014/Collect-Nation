94b4dc927d2526b39ab91d048f44ccb2fb1bf382
Alex Zhang <alexgzhang92@gmail.com>
2018-06-29 23:25:54 +0000

    Begin writing tests for controller_views/application_controller_view_spec.rb

--- spec/controller_views/application_controller_view_spec.rb
+++ spec/controller_views/application_controller_view_spec.rb
@@ -10,4 +10,11 @@ describe ApplicationController do
     end
   end
   
+  describe "Signup Page" do
+    it 'loads the signup page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+  end
+  
 end
