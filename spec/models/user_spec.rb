31e75d7b5da0dce6e1fa2c31e2dc9341a4784b32
Alex Zhang <alexgzhang92@gmail.com>
2018-06-29 23:02:55 +0000

    Add descriptions to user_spec.rb test file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "User" do
   before do
     @user = User.create(:username => "The Joker", :email => "jokerking50@gmail.com", :password => "jokerrules")
-    @fav_activities = Collection.create(:name => "Favorite Activities")
-    @fav_foods = Collection.create(:name => "Favorite Foods")
-    @robbing_banks = Item.create(:name => "Robbing Banks")
-    @laughing = Item.create(:name => "Laughing Maniacally")
-    @pizza = Item.create(:name => "Pizza")
-    @burger = Item.create(:name => "Burger")
+    @fav_activities = Collection.create(:name => "Favorite Activities", :description => "These are all the things I enjoy doing the most, even if some of them are illegal. But I'm the Joker, so what did you expect?")
+    @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!")
+    @robbing_banks = Item.create(:name => "Robbing Banks", :description => "I love money!")
+    @laughing = Item.create(:name => "Laughing Maniacally", :description => "Who doesn't love laughing? One day I'll make Batman laugh!")
+    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC")
+    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated")
   end
   
   it 'has a username' do
