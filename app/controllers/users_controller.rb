844b1f60ecfff8fc0699991da3098f4131e5ae68
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 21:17:39 +0000

    Add GET routes for homepage, signup, and login pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  get '/' do
+    erb :index
+  end
+  
+  get '/signup' do
+    erb :'users/signup'
+  end
+  
+  get '/login' do
+    erb :'users/login'
+  end
 end
