b588366561dc426f20c7cdc18662e133850ed391
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 22:29:01 +0000

    Modify get login route and create post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,20 @@ class UsersController < ApplicationController
   end
   
   get '/login' do
-    erb :'users/login'
+    if logged_in?
+      redirect to '/collections'
+    else
+      erb :'users/login'
+    end
+  end
+  
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/collections"
+    else
+      redirect to "/login"
+    end
   end
 end
