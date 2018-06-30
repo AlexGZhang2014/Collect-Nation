f88ea090074c9b9da4fb65cf6a238a603b0ad980
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 21:43:25 +0000

    Add tests for #slug and #find_by_slug methods to item model spec and verify they pass

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,6 +5,7 @@ describe "Item" do
     @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!")
     @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC", :collection_id => @fav_foods.id)
     @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated", :collection_id => @fav_foods.id)
+    @pad_thai = Item.create(:name => "Pad Thai", :description => "Thai food is great", :collection_id => @fav_foods.id)
   end
   
   it 'has a name and a description' do
@@ -12,6 +13,15 @@ describe "Item" do
     expect(@pizza.description).to eq("Best pizza is in NYC")
     expect(@burger.name).to eq("Burger")
     expect(@burger.description).to eq("In-N-Out is overrated")
+  end
+  
+  it 'can slug the name' do
+    expect(@pad_thai.slug).to eq("pad-thai")
+  end
+
+  it 'can find an item based on the slug' do
+    slug = @pad_thai.slug
+    expect(Item.find_by_slug(slug).name).to eq("Pad Thai")
   end
   
   it 'belongs to a collection' do
