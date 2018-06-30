4855620de3423d4e72bad9f94a51b3c1ab5c1411
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 22:57:33 +0000

    Define get users and get users/slug routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,4 +44,14 @@ class UsersController < ApplicationController
     session.clear
     redirect to "/login"
   end
+  
+  get '/users' do
+    @users = User.all
+    erb :'users/index'
+  end
+  
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 end
