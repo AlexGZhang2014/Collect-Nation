7dd91955a2b845569fa0e5324a2fa6131738242e
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 20:57:11 +0000

    Define #slug for collection and item classes and extend slugifiable::classmethods to both classes

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,12 @@
 class Collection < ActiveRecord::Base
   belongs_to :user
   has_many :items
+  
+  def slug
+    arr = self.name.split(" ")
+    new_arr = arr.collect {|word| word.downcase}
+    slug = new_arr.join("-")
+  end
+  
+  extend Slugifiable::ClassMethods
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :collection
+  
+  def slug
+    arr = self.name.split(" ")
+    new_arr = arr.collect {|word| word.downcase}
+    slug = new_arr.join("-")
+  end
+  
+  extend Slugifiable::ClassMethods
 end
