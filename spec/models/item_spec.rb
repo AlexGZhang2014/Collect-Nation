fa3157327d3ba6099af51002231fed6e3e6c3922
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 21:15:06 +0000

    Modify spec/models/item_spec.rb to have better written tests and verify all model specs are passing

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,20 +3,18 @@ require 'spec_helper'
 describe "Item" do
   before do
     @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!")
-    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC")
-    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated")
+    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC", :collection_id => @fav_foods.id)
+    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated", :collection_id => @fav_foods.id)
   end
   
   it 'has a name and a description' do
     expect(@pizza.name).to eq("Pizza")
     expect(@pizza.description).to eq("Best pizza is in NYC")
-    expect(@pizza.description).to eq("In-N-Out is overrated")
     expect(@burger.name).to eq("Burger")
+    expect(@burger.description).to eq("In-N-Out is overrated")
   end
   
   it 'belongs to a collection' do
-    @fav_foods.items << @pizza
-    @fav_foods.items << @burger
     expect(@fav_foods.items).to include(@pizza)
     expect(@fav_foods.items).to include(@burger)
   end
