f36de93330841548cfd6967b98fd78e8cbd4a25f
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 22:19:09 +0000

    Modify get signup route and create post signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,22 @@ class UsersController < ApplicationController
   end
   
   get '/signup' do
-    erb :'users/signup'
+    if logged_in?
+      redirect to '/collections'
+    else
+      erb :'users/signup'
+    end
+  end
+  
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/collections"
+    else
+      redirect to "/signup"
+    end
   end
   
   get '/login' do
