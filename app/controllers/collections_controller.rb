26dec08d1d4583d24bc06abc0dae8c6c15209ac5
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 22:41:54 +0000

    Add create collection, view users, and logout links to collection index page. Also defined get collection index method

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,10 @@
 class CollectionsController < ApplicationController
+  get '/collections' do
+    if logged_in?
+      @collections = Collection.all
+      erb :'collections/index'
+    else
+      redirect to '/login'
+    end
+  end
 end
