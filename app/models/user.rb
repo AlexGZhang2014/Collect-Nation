85f6cd01dea314890fcd7e4b4f4ccef7d3f087fa
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 22:23:11 +0000

    Add validations for username, email, and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :items, through: :collections
   has_secure_password
+  validates_presence_of :username, :email, :password
   
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
