19690edceabf50a61d04968127e00e9c93620792
Alex Zhang <alexgzhang92@gmail.com>
2018-06-30 21:12:06 +0000

    Modify models/user_spec.rb and models/collection_spec.rb tohave better written tests

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Collection" do
   before do
     @user = User.create(:username => "The Joker", :email => "jokerking50@gmail.com", :password => "jokerrules")
-    @fav_activities = Collection.create(:name => "Favorite Activities", :description => "These are all the things I enjoy doing the most, even if some of them are illegal. But I'm the Joker, so what did you expect?")
-    @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!")
-    @robbing_banks = Item.create(:name => "Robbing Banks", :description => "I love money!")
-    @laughing = Item.create(:name => "Laughing Maniacally", :description => "Who doesn't love laughing? One day I'll make Batman laugh!")
-    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC")
-    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated")
+    @fav_activities = Collection.create(:name => "Favorite Activities", :description => "These are all the things I enjoy doing the most, even if some of them are illegal. But I'm the Joker, so what did you expect?", :user_id => @user.id)
+    @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!", :user_id => @user.id)
+    @robbing_banks = Item.create(:name => "Robbing Banks", :description => "I love money!", :collection_id => @fav_activities.id)
+    @laughing = Item.create(:name => "Laughing Maniacally", :description => "Who doesn't love laughing? One day I'll make Batman laugh!", :collection_id => @fav_activities.id)
+    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC", :collection_id => @fav_foods.id)
+    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated", :collection_id => @fav_foods.id)
   end
   
   it 'has a name and a description' do
@@ -19,19 +19,19 @@ describe "Collection" do
   end
   
   it 'belongs to a user' do
-    @user.collections << @fav_activities
-    @user.collections << @fav_foods
+    #@user.collections << @fav_activities
+    #@user.collections << @fav_foods
     expect(@user.collections).to include(@fav_activities)
     expect(@user.collections).to include(@fav_foods)
   end
   
   it 'can have many items' do
-    @fav_activities.items << @robbing_banks
-    @fav_activities.items << @laughing
+    #@fav_activities.items << @robbing_banks
+    #@fav_activities.items << @laughing
     expect(@fav_activities.items).to include(@robbing_banks)
     expect(@fav_activities.items).to include(@laughing)
-    @fav_foods.items << @pizza
-    @fav_foods.items << @burger
+    #@fav_foods.items << @pizza
+    #@fav_foods.items << @burger
     expect(@fav_foods.items).to include(@pizza)
     expect(@fav_foods.items).to include(@burger)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "User" do
   before do
     @user = User.create(:username => "The Joker", :email => "jokerking50@gmail.com", :password => "jokerrules")
-    @fav_activities = Collection.create(:name => "Favorite Activities", :description => "These are all the things I enjoy doing the most, even if some of them are illegal. But I'm the Joker, so what did you expect?")
-    @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!")
-    @robbing_banks = Item.create(:name => "Robbing Banks", :description => "I love money!")
-    @laughing = Item.create(:name => "Laughing Maniacally", :description => "Who doesn't love laughing? One day I'll make Batman laugh!")
-    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC")
-    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated")
+    @fav_activities = Collection.create(:name => "Favorite Activities", :description => "These are all the things I enjoy doing the most, even if some of them are illegal. But I'm the Joker, so what did you expect?", :user_id => @user.id)
+    @fav_foods = Collection.create(:name => "Favorite Foods", :description => "These are all of my favorite foods!", :user_id => @user.id)
+    @robbing_banks = Item.create(:name => "Robbing Banks", :description => "I love money!", :collection_id => @fav_activities.id)
+    @laughing = Item.create(:name => "Laughing Maniacally", :description => "Who doesn't love laughing? One day I'll make Batman laugh!", :collection_id => @fav_activities.id)
+    @pizza = Item.create(:name => "Pizza", :description => "Best pizza is in NYC", :collection_id => @fav_foods.id)
+    @burger = Item.create(:name => "Burger", :description => "In-N-Out is overrated", :collection_id => @fav_foods.id)
   end
   
   it 'has a username' do
@@ -34,17 +34,11 @@ describe "User" do
   end
   
   it 'can have many collections' do
-    @user.collections << @fav_activities
-    @user.collections << @fav_foods
     expect(@user.collections).to include(@fav_activities)
     expect(@user.collections).to include(@fav_foods)
   end
   
   it 'can have many items through collections' do
-    @user.items << @robbing_banks
-    @user.items << @laughing
-    @user.items << @pizza
-    @user.items << @burger
     expect(@user.items).to include(@robbing_banks)
     expect(@user.items).to include(@laughing)
     expect(@user.items).to include(@pizza)
