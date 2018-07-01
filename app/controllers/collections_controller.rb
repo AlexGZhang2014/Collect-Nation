e365bfc40cfa9907142191a782d0f01e83262506
Alex Zhang <alexgzhang92@gmail.com>
2018-07-01 01:03:52 +0000

    Delete items/new.erb and define post collections method

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -15,4 +15,18 @@ class CollectionsController < ApplicationController
       redirect to '/login'
     end
   end
+  
+  post '/collections' do
+    if !params[:collection][:name].empty? && !params[:collection][:description].empty?
+      @collection = Collection.new(params[:collection])
+      @collection.user = current_user
+      if !params[:item][:name].empty? && !params[:item][:description].empty?
+        @collection.items << Item.create(params[:item])
+      end
+      @collection.save
+      redirect to "/collections/#{@collection.slug}"
+    else
+      redirect to "/collections/new"
+    end
+  end
 end
