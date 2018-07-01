23cdec09efcefe1bbc195e6441644a1ad2223ac4
Alex Zhang <alexgzhang92@gmail.com>
2018-07-01 18:44:24 +0000

    Define get items/:slug/edit route in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,17 @@ class ItemsController < ApplicationController
       redirect to '/login'
     end
   end
+  
+  get '/items/:slug/edit' do
+    @item = Item.find_by_slug(params[:slug])
+    if logged_in? && current_user.id == @item.collection.user.id
+      erb :'items/edit'
+    elsif logged_in?
+      redirect to "/collections"
+    else
+      redirect to '/login'
+    end
+  end
+  
+  
 end
