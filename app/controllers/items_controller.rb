ae23e652443116c6eb8b1709af31e3a631046f95
Alex Zhang <alexgzhang92@gmail.com>
2018-07-01 18:19:19 +0000

    Define get items/:slug route in items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  get '/items/:slug' do
+    if logged_in?
+      @item = Item.find_by_slug(params[:slug])
+      erb :'items/show'
+    else
+      redirect to '/login'
+    end
+  end
 end
