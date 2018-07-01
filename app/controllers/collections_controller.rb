ce9f2b3d7aadc0401df6c46ebfc58cb791191bc9
Alex Zhang <alexgzhang92@gmail.com>
2018-07-01 01:47:43 +0000

    Define patch collections/:slug route

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -47,4 +47,18 @@ class CollectionsController < ApplicationController
       redirect to "/login"
     end
   end
+  
+  patch '/collections/:slug' do
+    @collection = Collection.find_by_slug(params[:slug])
+    if !params[:collection][:name].empty? && !params[:collection][:description].empty? && !params[:item][:name].empty? && !params[:item][:description].empty?
+      @collection.update(name: params[:collection][:name], description: params[:collection][:description])
+      @collection.items << Item.create(params[:item])
+      @collection.save
+      redirect to "/tweets/#{@tweet.id}"
+    elsif !params[:collection][:name].empty? && !params[:collection][:description].empty?
+      @collection.update(name: params[:collection][:name], description: params[:collection][:description])
+    else
+      redirect to "/collections/#{@collection.slug}/edit"
+    end
+  end
 end
