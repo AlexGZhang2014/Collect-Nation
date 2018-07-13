95af9ea8067505761b0c356a28a84165b653beb8
Alex Zhang <alexgzhang92@gmail.com>
2018-07-13 14:01:04 +0000

    Add validation for uniqueness of username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :items, through: :collections
   has_secure_password
   validates_presence_of :username, :email, :password
+  validates_uniqueness_of :username, :email
   
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
